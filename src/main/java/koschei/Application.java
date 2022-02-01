a599ec907abc3578170a4f0384100fea2892006c
elena-fly17 <elena17mr@gmail.com>
2022-02-01 19:29:11 +0200

    Задача решена

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -1,15 +1,17 @@
 package koschei;
-
 import koschei.config.AppConfig;
+import koschei.models.Ocean1;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
+        AnnotationConfigApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         KoscheiTheDeathless koscheiTheDeathless =
                 applicationContext.getBean(KoscheiTheDeathless.class);
+
         System.out.println(koscheiTheDeathless.getRulesByDeth());
+        applicationContext.close();
     }
 }
--- src/main/java/koschei/KoscheiTheDeathless.java
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -1,12 +1,10 @@
 package koschei;
-
 import koschei.models.Ocean1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KoscheiTheDeathless {
-
     private Ocean1 ocean;
 
     public String getRulesByDeth() {
